65b39240d47151f5e7b47967cb8e9e4ffd0c8dc3
mirabellee <agnieszkakotowicz88@gmail.com>
2016-09-10 18:13:30 +0200

    Index and detail templates were added. Admin site was added. URLs were changed to give a namespace.

--- env/mysite/polls/urls.py
+++ env/mysite/polls/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls' # it is a namespace for this application URLs
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/', views.about, name='about')
+    url(r'^about/', views.about, name='about'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
 ]
